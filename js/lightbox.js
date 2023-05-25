44b849b9a176c05853736240624fef006dc4940d
davidpor <davidp@itcsa.net.ar>
2023-05-25 19:00:52 -0300

    formulario y correcciones

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,4 +1,5 @@
-const imagenes = document.querySelectorAll('.img-galeria')
+//Declaro las constantes
+const imagenes = document.querySelectorAll('.img-galeria') //captura las imagenes en la constatnte
 const imagenLight = document.querySelector('.agregar-imagen');
 const contenedorLight = document.querySelector('.imagen-light')
 const closeLight = document.querySelector('.close')
@@ -9,12 +10,12 @@ imagenes.forEach(imagen => {
         aparecerImagen(imagen.getAttribute('src'));
     })
 });
-
+//para que al hacer click fuera de la imagen o el equis se cierra
 contenedorLight.addEventListener('click',(e)=>{
     if(e.target !== imagenLight){
         contenedorLight.classList.toggle('show')
         imagenLight.classList.toggle('showImage')
-        hamburguer.style.opacity = '1';
+        hamburguer.style.opacity = '1'; //para que aparezca el icono del menu, cuando se cierra la imagen
     }
 })
 
@@ -23,5 +24,5 @@ const aparecerImagen = (imagen)=>{
     imagenLight.src = imagen;
     contenedorLight.classList.toggle('show')
     imagenLight.classList.toggle('showImage')
-    hamburguer.style.opacity = '0';
+    hamburguer.style.opacity = '0'; //para hacer desaparecer el icono del menu, cuando se habre la imagen
 }
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,12 @@
+// Declaro las constantes
 const hamburguer = document.querySelector('.hamburguer')
 const menu = document.querySelector('.menu-navegacion')
 
-
+// Agrega o quita la clase spread, para dar el efecto del despliegue del menu
 hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
 })
-
+//Este codigo permite cerrar el menu (quita la clase spread) al hacer click fuera de la barra de menu
 window.addEventListener('click', e =>{
     if(menu.classList.contains('spread') 
         && e.target != menu && e.target != hamburguer){
